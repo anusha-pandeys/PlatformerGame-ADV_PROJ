4a2e1f84415046d4331aacda6ef5949d46b6a4c9
ABHIGNA NIMMAGADDA <s-ANIMMAGADDA@lwsd.org>
2023-10-31 10:40:41 -0700

    Added player entity

--- minimalist-game-framework-core/Game/entity.cs
+++ minimalist-game-framework-core/Game/entity.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Game.Game
+internal interface entity
 {
-    internal interface entity
-    {
-        void Render();
-        void DrawRectangle(Vector2 position, Vector2 size, Color color);
-    }
+    void Render();
+    void DrawRectangle(Vector2 position, Vector2 size, Color color);
 }
