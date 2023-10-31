3a3fab69234d21f7405e388d3794074993c86371
ABHIGNA NIMMAGADDA <s-ANIMMAGADDA@lwsd.org>
2023-10-31 10:15:47 -0700

    adding entity interface

---
+++ minimalist-game-framework-core/Game/entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Game
+{
+    internal interface entity
+    {
+        void Render();
+        void DrawRectangle(Vector2 position, Vector2 size, Color color);
+    }
+}
