98423287df564c17082918e182d906bc34fa7a0e
ABHIGNA NIMMAGADDA <s-ANIMMAGADDA@lwsd.org>
2023-11-01 17:38:29 -0700

    Added the background color + coordinates of the player using a text renderer class.

--- minimalist-game-framework-core/Game/Game.cs
+++ minimalist-game-framework-core/Game/Game.cs
@@ -6,12 +6,15 @@ class Game
     public static readonly string Title = "Minimalist Game Framework";
     public static readonly Vector2 Resolution = new Vector2(640, 480);
     private List<Entity> entities = new List<Entity>();
+    private TextRenderer textRenderer;
+    Font font = Engine.LoadFont("Retro Gaming.ttf", 11);
 
     Player x;
     Map map;
     Blocks floor;
     public Game()
     {
+        Font font = Engine.LoadFont("Retro Gaming.ttf", 11);
         Vector2 playerPosition = new Vector2(100, 100); // Initial position
         Vector2 playerVelocity = new Vector2(0, 0);     // Initial velocity
         map = new Map();
@@ -19,6 +22,7 @@ class Game
         floor = new Blocks(new Vector2(300, 400), new Vector2(50, 50), GameColor.Block1);
         entities.Add(x);
         entities.Add(floor);
+        textRenderer = new TextRenderer();
     }
 
     public void Update()
@@ -26,5 +30,13 @@ class Game
         map.setBackgroundColor();
         floor.Render();
         x.playerLoop();
+
+        DisplayPlayerCoordinates();
+    }
+
+    public void DisplayPlayerCoordinates()
+    {
+        string playerCoordinates = string.Format("{0}, {1}", x.getCoordinates()[0].X, x.getCoordinates()[0].Y);
+        textRenderer.displayText(playerCoordinates, new Vector2(0, 0), Color.Black, font);
     }
 }
