3afb2bb440ce5b08e9ec87fd461e0c836061d213
VAIBHAV ATTRE <s-vattre@lwsd.org>
2023-11-17 10:41:20 -0800

    Implementing collision object

--- minimalist-game-framework-core/Game/Collision/CollisionManager.cs
+++ minimalist-game-framework-core/Game/Collision/CollisionManager.cs
@@ -17,7 +17,7 @@ internal class CollisionManager
         return collidable;
     }
 
-    public static void addBlock(Blocks block)//
+    public static void addBlock(Blocks block)
     {
         blocks.Add(block);
     }
@@ -84,22 +84,13 @@ internal class CollisionManager
         Rectangle rectA = a.Bound;
         Rectangle rectB = b.Bound;
 
-        /*Vector2 boundALeft = new Vector2(rectA.X, rectA.Y);
-        Vector2 boundARight = new Vector2(rectA.X, rectA.Y) + new Vector2(rectA.Width, rectA.Width);
-        Vector2 boundBLeft = new Vector2(rectB.X, rectB.Y);
-        Vector2 boundBRight = new Vector2(rectB.X, rectB.Y) + new Vector2(rectB.Width, rectB.Width);
-
-        if (boundARight.X < boundBLeft.X || boundBRight.X < boundALeft.X ||
-                boundARight.Y < boundBLeft.Y || boundBRight.Y < boundALeft.Y)
-            return false;
-        else return true;*/
         Dictionary<string, bool> retMap = isCollided(rectA, rectB);
         return retMap;
     }
 
     public static Dictionary<string, bool> isCollided(Rectangle rectA, Rectangle rectB)
     {
-        Dictionary<string, bool> retMap = new Dictionary<string, bool>();
+        CollisionObject collisions = new CollisionObject();
         bool upOrDown = false;
         bool rightLeft = false;
         Vector2 boundALeft = new Vector2(rectA.X, rectA.Y);
@@ -107,39 +98,40 @@ internal class CollisionManager
         Vector2 boundBLeft = new Vector2(rectB.X, rectB.Y);
         Vector2 boundBRight = new Vector2(rectB.X, rectB.Y) + new Vector2(rectB.Width, rectB.Width);
 
-        if (rectA.Y < rectB.Y)
+        if ((rectA.Y+(rectA.Height/2)) < (rectB.Y + (rectB.Height / 2)))
         {
-            retMap["down"] = true;
+            //retMap["down"] = true;
+            collisions.setDown(true);
             upOrDown = true;
         } 
-        if (rectA.Y > rectB.Y)
+        if ((rectA.Y + (rectA.Height / 2)) > (rectB.Y + (rectB.Height / 2)))
         {
-            retMap["up"] = true;
+            collisions.setUp(true);
             upOrDown = false;
         }
-        if(rectA.X < rectB.X)
+        if((rectA.X + (rectA.Width / 2)) < (rectB.X + (rectB.Width / 2)))
         {
-            retMap["right"] = true;
+            collisions.setRight(true);
             rightLeft = true;
         } 
-        if (rectA.X > rectB.X)
+        if ((rectA.X + (rectA.Width / 2)) > (rectB.X + (rectB.Width / 2)))
         {
-            retMap["left"] = true;
+            collisions.setLeft(true);
             rightLeft = false;
         }
 
         if (boundARight.X < boundBLeft.X || boundBRight.X < boundALeft.X ||
                 boundARight.Y < boundBLeft.Y || boundBRight.Y < boundALeft.Y)//
         {
-            if(upOrDown && retMap.ContainsKey("down"))
+            if(upOrDown && collisions.getDown())
             {
-                retMap["down"] = false;
-            } else if (!upOrDown && retMap.ContainsKey("up"))
+                collisions.setDown(false);
+            } else if (!upOrDown && collisions.getDown())
             {
-                retMap["up"] = false;
+                collisions.setUp(false);
             }
-            if(rightLeft && retMap.ContainsKey("right")) {
-                retMap["right"] = false;
+            if(rightLeft && collisions.getRight()) {
+                collisions.setRight = false;
             } else if (!rightLeft && retMap.ContainsKey("left"))
             {
                 retMap["left"] = false;
---
+++ minimalist-game-framework-core/Game/Collision/CollisionObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+internal class CollisionObject
+{
+
+    private bool left;
+    private bool right;
+    private bool up;
+    private bool down;
+
+    public CollisionObject()
+    {
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+    }
+
+    public bool getLeft()
+    {
+        return left;
+    }
+    public bool getRight()
+    {
+        return right;
+    }
+
+    public bool getUp()
+    {
+        return up;
+    }
+
+    public bool getDown()
+    {
+        return down;
+    }
+    public void setLeft(bool left)
+    {
+        this.left = left;
+    }
+    public void setRight(bool right)
+    {
+        this.right = right;
+    }
+
+    public void setUp(bool up)
+    {
+        this.up = up;
+    }
+
+    public void setDown(bool down)
+    {
+        this.down = down;
+    }
+}
