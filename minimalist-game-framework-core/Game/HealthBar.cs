f66da5d307a02d022436976bf13ce01429c45c80
VAIBHAV ATTRE <s-vattre@lwsd.org>
2023-11-27 10:41:08 -0800

    Health bar implememntation

---
+++ minimalist-game-framework-core/Game/HealthBar.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SDL2;
+
+internal class HealthBar : UIElements
+{
+    private string name;
+    private Vector2 position;
+    private int health;
+    private IntPtr Renderer => Engine.Renderer2;
+    public HealthBar(string name, Vector2 position)
+    {
+        setName(name);
+        setPosition(position);
+        health = 100;
+    }
+
+    public void setHealth(int health)
+    {
+        this.health = health;
+        Draw();
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+    public override string getName()
+    {
+        return name;
+    }
+    public override void setName(string name)
+    {
+        this.name = name;
+    }
+    public override Vector2 getPosition()
+    {
+        return position;
+    }
+    public override void setPosition(Vector2 position)
+    {
+        this.position = position;
+    }
+
+    public override void Render(Camera camera)
+    {
+        Draw(position, new Vector2(200, 50));
+    }
+
+
+    protected override void Draw(Vector2 position, Vector2 size)
+    {
+        SDL.SDL_SetRenderDrawColor(Renderer, 205, 133, 63, 100);
+
+        SDL.SDL_Rect rect = new SDL.SDL_Rect()
+        {
+            x = (int)position.X,
+            y = (int)position.Y,
+            w = (int)size.X,
+            h = (int)size.Y
+        };
+
+        SDL.SDL_RenderFillRect(Renderer, ref rect);
+
+        SDL.SDL_SetRenderDrawColor(Renderer, 220, 20, 60, 100);
+
+        SDL.SDL_Rect rect2 = new SDL.SDL_Rect()
+        {
+            x = (int)position.X+10,
+            y = (int)position.Y+10,
+            w = (int)(size.X*(health/100))-10,
+            h = (int)size.Y-10
+        };
+
+        SDL.SDL_RenderFillRect(Renderer, ref rect);
+    }
+}
---
+++ minimalist-game-framework-core/Game/UIElements.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal abstract class UIElements
+{
+    public abstract void setName(string name);
+    public abstract string getName();
+    public abstract void setPosition(Vector2 position);
+    public abstract Vector2 getPosition();
+    public abstract void Render(Camera camera);
+    protected abstract void Draw(Vector2 position, Vector2 size);
+}
