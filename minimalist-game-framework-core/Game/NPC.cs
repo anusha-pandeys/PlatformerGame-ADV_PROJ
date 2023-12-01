eb60f9dc713b193b78e353c3fa109beea86de2b3
s-vattre <113128715+s-vattre@users.noreply.github.com>
2023-11-30 17:35:41 -0800

    Healthbar

--- minimalist-game-framework-core/Game/NPC.cs
+++ minimalist-game-framework-core/Game/NPC.cs
@@ -39,7 +39,7 @@ internal class NPC : Entity
 
         // Check if enough time has passed since the last collision to revert to the original color
         if (timeSinceCollision >= collisionCooldown)
-        {
+        {//
             npcColor = originalColor;
         }
     }
